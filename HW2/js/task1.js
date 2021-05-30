42495d65cda2e9df02157178fa646235f30de6ca
Valeriya <elka_5@ukr.net>
2021-05-30 19:39:13 +0300

    edit

--- HW2/js/task1.js
+++ HW2/js/task1.js
@@ -1,11 +1,11 @@
 console.log("===== Task 1 =====")
-let a = prompt("Введите число")
-let b = prompt("Введите число")
-let c = prompt("Введите число")
+let a = +prompt("Введите число")
+let b = +prompt("Введите число")
+let c = +prompt("Введите число")
 if (a==b || b==c || c==a) {
     alert("Error");
 } else {
-    var varage = ((+a + +b+ +c)/3);
-alert (`Ваше среднее число ${varage}`)
-console.log (`Ваше среднее число ${varage}`);
+    let avarage = (a+b+c)/3;
+alert (`Ваше среднее число ${avarage}`)
+console.log (`Ваше среднее число ${avarage}`);
 }
